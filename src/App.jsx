1093aa4b88979cbb32b49eec8a9e1fcea20c3621
P.A.J.U.Pallewela <janithup8@gmail.com>
2025-05-16 22:59:01 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,18 @@
-import Header from './components/Header/Header.jsx';
-import { CORE_CONCEPTS } from './data';
-import CoreConcept from './components/CoreConcept/CoreConcept.jsx'
-import TabButton from './components/TabButton/TabButton.jsx';
-import { EXAMPLES } from './data';
-import { useState } from 'react';
+import Player from './components/Player.jsx';
+import TimerChallenge from './components/TimerChallenge.jsx';
+
 function App() {
-
-  const [selectedTopic, setSelectedTopic] = useState();
-
-  function handleSelect(selectedButton) {
-    setSelectedTopic(selectedButton);
-
-  }
-
-  let tabContent = <p>Please select a topic.</p>;
-
-  if (selectedTopic) {
-    tabContent = (<div id="tab-content">
-      <h3>{EXAMPLES[selectedTopic].title}</h3>
-      <p>{EXAMPLES[selectedTopic].description}</p>
-      <pre>
-        <code>{EXAMPLES[selectedTopic].code}</code>
-      </pre>
-    </div>);
-  }
-
   return (
-    <div>
-      <Header />
-
-      <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            {CORE_CONCEPTS.map((conceptItem) => <CoreConcept key={conceptItem.title}  {...conceptItem} />)}
-
-
-          </ul>
-        </section>
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-
-            <TabButton isSelected={selectedTopic === 'components'} onSelect={() => handleSelect('components')}>Components</TabButton>
-            <TabButton isSelected={selectedTopic === 'jsx'} onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-            <TabButton isSelected={selectedTopic === 'props'} onSelect={() => handleSelect('props')}>Props</TabButton>
-            <TabButton isSelected={selectedTopic === 'state'} onSelect={() => handleSelect('state')}>State</TabButton>
-          </menu>
-
-          {tabContent}
-        </section>
-      </main>
-    </div>
+    <>
+      <Player />
+      <div id="challenges">
+        <TimerChallenge title="Easy" targetTime={1} />
+        <TimerChallenge title="Not easy" targetTime={5} />
+        <TimerChallenge title="Getting tough" targetTime={10} />
+        <TimerChallenge title="Pros only" targetTime={15} />
+       
+      </div>
+    </>
   );
 }
 
